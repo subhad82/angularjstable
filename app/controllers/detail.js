a0b9a55e7ccd40dc7f7277d6a6e69c1fd0fa999a
subhadeep <subhad82@gmail.com>
2018-05-06 14:49:53 +0530

    addup

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -12,20 +12,28 @@ myApp.controller('detailCtrl',['$scope','$state','$location',function($scope,$st
 
 
      $scope.people = [
-    {name:'Jon', join: "5/8/18 12:00 AM", title: 'Developer'},
-    {name:'Mike', join: "5/8/18 12:00 AM", title: 'Manager'},{name:'Jon', join: "5/8/18 12:00 AM", title: 'Developer'},
-    {name:'Mike', join: "5/8/18 12:00 AM", title: 'Manager'},
-    {name:'Allen', join: "5/8/18 12:00 AM", title: 'CEO'}
+    {name:'Jon', join: "5/8/18 12:00 AM", id: '2AG6B7', status : 'active', section1:'1', section2 :'0' , section3 : '1' , section4 :'0' },
+    {name:'Mike', join: "5/8/18 12:00 AM", id: 'K8F85K' ,status : 'active' , section1:'0', section2 :'0' , section3 : '1' , section4 :'0'},
+     {name:'Jon', join: "5/8/18 12:00 AM", id: 'J80V67' ,status : 'active' , section1:'1', section2 :'0' , section3 : '0' , section4 :'0'},
+    {name:'Mike', join: "5/8/18 12:00 AM", id: '789BHG' ,status : 'active' , section1:'1', section2 :'1' , section3 : '1' , section4 :'1'},
+    {name:'Allen', join: "5/8/18 12:00 AM", id: 'J9G78N' ,status : 'active' , section1:'0', section2 :'1' , section3 : '1' , section4 :'0'}
     ];
     
   $scope.addPerson = function(){
     var person = {
         name: $scope.name,
         join: $scope.join,
-        title: $scope.title,
+        id: $scope.id,
+        status : $scope.status,
+        section1 : $scope.section1,
+        section2 : $scope.section2,
+        section3 : $scope.section3,
+        section4 : $scope.section4,
+        
     };
     
     $scope.people.push(person);
+     
   };
    
    $scope.removePerson = function(index){
